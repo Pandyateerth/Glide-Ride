c20d31a64cbcb78acbe000baf7f48441420f407e
Teerth Pandya <teerthpandya2742002@gamil.com>
2025-03-05 18:52:40 -0500

    create react app and pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<UserLogin />} />
+      <Route path='/signup' element={<UserSignup />} />
+      <Route path='/captain-login' element={<Captainlogin />} />
+      <Route path='/captain-signup' element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CaptainSignup = () => {
+  return (
+    <div>
+        CaptainSignup
+      
+    </div>
+  )
+}
+
+export default CaptainSignup
