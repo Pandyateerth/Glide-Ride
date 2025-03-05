e9472263625158c314373a971d501b04c6e2ff0b
Teerth Pandya <teerthpandya2742002@gamil.com>
2025-03-05 17:23:21 -0500

    user registration backend done

--- glide-ride-backend/app.js
+++ glide-ride-backend/app.js
@@ -3,6 +3,10 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+connectToDb();
 
 
 app.use(cors());
@@ -11,5 +15,7 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello Teerth Pandya');
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
